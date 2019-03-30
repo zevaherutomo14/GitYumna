d669bf3b107bd64fb68a5287ab6446343edd2ad3
Muh Fadhil Al Haaq Ginoga <dilloginoga@gmail.com>
2019-03-30 10:05:21 +0700

    Update activity kuis

---
+++ app/src/main/java/wardiman/com/yumna/Goals/activity_kuissyahadat.java
@@ -0,0 +1,15 @@
+package wardiman.com.yumna.Goals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import wardiman.com.yumna.R;
+
+public class activity_kuissyahadat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuissyahadat);
+    }
+}
