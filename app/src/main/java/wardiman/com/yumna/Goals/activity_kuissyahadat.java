3aecdb43c76e3ee66b754ec443352c576f57bce7
Diman12345 <wardimanperdian@gmail.com>
2019-04-16 00:21:27 +0700

    Template Quiz
    Penambahan fitur Quiz untuk syahadat

--- app/src/main/java/wardiman/com/yumna/Goals/activity_kuissyahadat.java
+++ app/src/main/java/wardiman/com/yumna/Goals/activity_kuissyahadat.java
@@ -1,15 +1,42 @@
 package wardiman.com.yumna.Goals;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import wardiman.com.yumna.Quiz.EssayActivity;
+import wardiman.com.yumna.Quiz.PilGanSy;
+import wardiman.com.yumna.Quiz.PilihanGandaActivity;
 import wardiman.com.yumna.R;
 
 public class activity_kuissyahadat extends AppCompatActivity {
+
+    LinearLayout llPilGan, llEssay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuissyahadat);
+
+        llEssay =  findViewById(R.id.essay);
+        llPilGan =  findViewById(R.id.pilGan);
+
+        llPilGan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_kuissyahadat.this, PilGanSy.class);
+                startActivity(intent);
+            }
+        });
+
+        llEssay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_kuissyahadat.this, EssayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/wardiman/com/yumna/Quiz/SoalPilihanGanda.java
+++ app/src/main/java/wardiman/com/yumna/Quiz/SoalPilihanGanda.java
@@ -15,7 +15,32 @@ public class SoalPilihanGanda {
 
     };
 
+    public String syahadat[] = {
+            "XXXSiapa nama presiden Indonesia yang pertama ?",
+            "XXXIdeologi dasar bagi negara Indonesia adalah ...",
+            "XXXBhinneka Tunggal Ika mempunyai arti ...",
+            "XXXIbukota negara Indonesia saat ini adalah ...",
+            "XXXSiapa yang menjajah Indonesia selama 350 tahun ?",
+            "XXXSaat masa penjajahan, senjata yang biasa digunakan oleh pahlawan Indonesia adalah ...",
+            "XXXMonumen untuk mengenang perlawanan dan perjuanagan rakyat Indonesia untuk merebut kemerdekaan dari pemerintahan kolonial Hindia Belanda adalah ...",
+            "XXXTeks yang dibacakan Ir. Soekarno yang menyatakan Indonesia merdeka dalah teks ...",
+            "XXXPulau terbesar di Indonesia adalah ...",
+
+    };
+
     private String PilihanJawaban[][] = {
+            {"Ir. Soekarno","Joko Widodo","Susilo Bambang Yudhoyono"},
+            {"UUD 1945","Pancasila","Bhinneka Tunggal Ika"},
+            {"Berbeda-beda tetapi tetap satu","Bersama selamanya","Bersatu teguh bercerai runtuh"},
+            {"Semarang","Surabaya","Jakarta"},
+            {"Jepang","Belanda","Malaysia"},
+            {"Bambu runcing","Ketapel","Shotgun"},
+            {"Tugu Muda","Patung Pancoran","Monas"},
+            {"Proklamasi","Pancasila","Pembukaan UUD 1945"},
+            {"Jawa","Sumatera","Kalimantan"},
+    };
+
+    private String PilJwbSy[][] = {
             {"Ir. Soekarno","Joko Widodo","Susilo Bambang Yudhoyono"},
             {"UUD 1945","Pancasila","Bhinneka Tunggal Ika"},
             {"Berbeda-beda tetapi tetap satu","Bersama selamanya","Bersatu teguh bercerai runtuh"},
@@ -39,14 +64,36 @@ public class SoalPilihanGanda {
             "Kalimantan",
     };
 
+    private String JwbSy[] = {
+            "Ir. Soekarno",
+            "Pancasila",
+            "Berbeda-beda tetapi tetap satu",
+            "Jakarta",
+            "Belanda",
+            "Bambu runcing",
+            "Monas",
+            "Proklamasi",
+            "Kalimantan",
+    };
+
     public String getPertanyaan(int x) {
         String soal =  pertanyaan[x];
         return soal;
     }
 
+    public String getSyahadat(int x){
+        String soalsy = syahadat[x];
+        return soalsy;
+    }
+
     public String getJawabanBenar(int x) {
         String jawaban = jawabanBenar[x];
         return jawaban;
+    }
+
+    public String getJwbBnrSyahadat(int x) {
+        String jwbSy = JwbSy[x];
+        return jwbSy;
     }
 
     public String getPilihanJawaban1(int x) {
@@ -62,6 +109,21 @@ public class SoalPilihanGanda {
     public String getPilihanJawaban3(int x) {
         String jawaban3 = PilihanJawaban[x][2];
         return jawaban3;
+    }
+
+    public String getPilJwbSy1(int x) {
+        String jwbSy1 = PilJwbSy[x][0];
+        return jwbSy1;
+    }
+
+    public String getPilJwbSy2(int x) {
+        String jwbSy2 = PilJwbSy[x][1];
+        return jwbSy2;
+    }
+
+    public String getPilJwbSy3(int x) {
+        String jwbSy3 = PilJwbSy[x][2];
+        return jwbSy3;
     }
 
 }
