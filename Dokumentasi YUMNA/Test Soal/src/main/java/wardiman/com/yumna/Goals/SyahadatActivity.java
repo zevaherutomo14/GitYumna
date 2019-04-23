02db538a511ec249ad5c870ac242b1ea8f272222
aji fajar nugraha <ajifajar23@gmail.com>
2019-04-24 00:27:42 +0700

    test soal pilgan
    mohon untuk di coba terlebih dahulu

---
+++ Dokumentasi YUMNA/Test Soal/src/main/java/wardiman/com/yumna/Goals/SyahadatActivity.java
@@ -0,0 +1,47 @@
+package wardiman.com.yumna.Goals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import wardiman.com.yumna.R;
+
+public class SyahadatActivity extends AppCompatActivity {
+
+    Button btn_kuissyahadat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syahadat);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        btn_kuissyahadat = (Button) findViewById(R.id.btn_kuissyahadat);
+        btn_kuissyahadat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SyahadatActivity.this, activity_kuissyahadat.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
