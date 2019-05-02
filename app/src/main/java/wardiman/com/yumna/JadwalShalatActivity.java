8d51c00c6e2cd57282152ed9eae86faf99bbbb51
DilloG <dilloginoga@gmail.com>
2019-05-03 01:50:54 +0700

    Jadwal shalat implementednew interface

--- app/src/main/java/wardiman/com/yumna/JadwalShalatActivity.java
+++ app/src/main/java/wardiman/com/yumna/JadwalShalatActivity.java
@@ -13,7 +13,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,8 +26,10 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -40,9 +45,11 @@ import wardiman.com.yumna.network.ApiUrl;
 public class JadwalShalatActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
 
     String zuhur, ashar, magrib, isya, subuh, tanggal;
+    CardView card_view_subuh, card_view_dzuhur, card_view_ashar, card_view_maghrib, card_view_isya;
     List<Item> jadwal;
     private TextView tv_lokasi_value, tv_fajr_value, tv_shurooq_value,
-            tv_dhuhr_value, tv_asr_value, tv_maghrib_value, tv_isha_value, tv_jadwal;
+            tv_dhuhr_value, tv_asr_value, tv_maghrib_value, tv_isha_value, tv_jadwal,
+            info_text_subuh, info_text_dzuhur, info_text_ashar, info_text_isya, info_text_maghrib;
     private ProgressDialog progressDialog;
     Location location;
     String lokasi;
@@ -50,6 +57,8 @@ public class JadwalShalatActivity extends AppCompatActivity implements EasyPermi
     Calendar calander;
     SimpleDateFormat simpledateformat;
     String Date;
+    SimpleDateFormat simpletimeformat;
+    String Time;
 
     private FloatingActionButton fab_refresh;
 
@@ -57,6 +66,25 @@ public class JadwalShalatActivity extends AppCompatActivity implements EasyPermi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal_shalat);
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        toolbar.setTitle("Jadwal Shalat");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        card_view_subuh = (CardView) findViewById(R.id.card_view_subuh);
+        card_view_dzuhur = (CardView) findViewById(R.id.card_view_dzuhur);
+        card_view_ashar = (CardView) findViewById(R.id.card_view_ashar);
+        card_view_maghrib= (CardView) findViewById(R.id.card_view_maghrib);
+        card_view_isya = (CardView) findViewById(R.id.card_view_isya);
+
+        info_text_subuh = (TextView) findViewById(R.id.info_text_subuh);
+        info_text_dzuhur = (TextView) findViewById(R.id.info_text_dzuhur);
+        info_text_ashar = (TextView) findViewById(R.id.info_text_ashar);
+        info_text_maghrib = (TextView) findViewById(R.id.info_text_maghrib);
+        info_text_isya = (TextView) findViewById(R.id.info_text_isya);
 
         tv_lokasi_value = (TextView) findViewById(R.id.tv_lokasi_value);
         tv_fajr_value = (TextView)findViewById(R.id.tv_fajr_value);
@@ -80,6 +108,15 @@ public class JadwalShalatActivity extends AppCompatActivity implements EasyPermi
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 //    private void refresh() {
 //        swipeId.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
 //            @Override
@@ -137,10 +174,13 @@ public class JadwalShalatActivity extends AppCompatActivity implements EasyPermi
 
                                 if (lokasi != null) {
 
-
+                                    //get time format and get time
                                     calander = Calendar.getInstance();
                                     simpledateformat = new SimpleDateFormat("dd-MM-yyyy");
                                     Date = simpledateformat.format(calander.getTime());
+
+                                    simpletimeformat = new SimpleDateFormat("hh:mm a");
+                                    Time = simpletimeformat.format(calander.getTime());
 
                                     //melakukan get melalui API
                                     ApiServices apiInterface = ApiUrl.getClient().create(ApiServices.class);
@@ -169,6 +209,48 @@ public class JadwalShalatActivity extends AppCompatActivity implements EasyPermi
                                                     tv_lokasi_value.setText(lokasi);
                                                     tv_jadwal.setText(Date);
                                                     progressDialog.dismiss();
+
+                                                    //set color
+                                                    SimpleDateFormat simplezuhurformat = new SimpleDateFormat("hh:mm a");
+                                                    try {
+                                                        Date datesubuh = simplezuhurformat.parse(subuh);
+                                                        Date datezuhur = simplezuhurformat.parse(zuhur);
+                                                        Date dateashar = simplezuhurformat.parse(ashar);
+                                                        Date datemagrib = simplezuhurformat.parse(magrib);
+                                                        Date dateisya = simplezuhurformat.parse(isya);
+
+                                                        Date dateasli = simpletimeformat.parse(Time);
+
+                                                        if(dateisya.after(dateasli)){
+                                                            card_view_isya.setCardBackgroundColor(getResources().getColor(R.color.shalatTime));
+                                                            tv_isha_value.setTextColor(getResources().getColor(R.color.white));
+                                                            info_text_isya.setTextColor(getResources().getColor(R.color.white));
+                                                        }
+                                                        else if(datemagrib.after(dateasli)){
+                                                            card_view_maghrib.setCardBackgroundColor(getResources().getColor(R.color.shalatTime));
+                                                            tv_maghrib_value.setTextColor(getResources().getColor(R.color.white));
+                                                            info_text_maghrib.setTextColor(getResources().getColor(R.color.white));
+                                                        }
+                                                        else if(dateashar.after(dateasli)){
+                                                            card_view_ashar.setCardBackgroundColor(getResources().getColor(R.color.shalatTime));
+                                                            tv_asr_value.setTextColor(getResources().getColor(R.color.white));
+                                                            info_text_ashar.setTextColor(getResources().getColor(R.color.white));
+                                                        }
+                                                        else if(datezuhur.after(dateasli)){
+                                                            card_view_dzuhur.setCardBackgroundColor(getResources().getColor(R.color.shalatTime));
+                                                            tv_dhuhr_value.setTextColor(getResources().getColor(R.color.white));
+                                                            info_text_dzuhur.setTextColor(getResources().getColor(R.color.white));
+                                                        }
+                                                        else if(datesubuh.after(dateasli)){
+                                                            card_view_subuh.setCardBackgroundColor(getResources().getColor(R.color.shalatTime));
+                                                            tv_fajr_value.setTextColor(getResources().getColor(R.color.white));
+                                                            info_text_subuh.setTextColor(getResources().getColor(R.color.white));
+                                                        }
+
+                                                    } catch (ParseException e) {
+                                                        e.printStackTrace();
+                                                    }
+
                                                 } else {
                                                     Toast.makeText(JadwalShalatActivity.this, "Error Network", Toast.LENGTH_SHORT).show();
                                                     progressDialog.dismiss();
