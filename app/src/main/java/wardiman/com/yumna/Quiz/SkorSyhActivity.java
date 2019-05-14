331b2c3ce8c5d58999b0aeec73b176b941694bd9
Diman12345 <wardimanperdian@gmail.com>
2019-05-15 03:07:02 +0700

    Update Activity
    Penambahan Activity utk melengkapi Quiz masih kosong

---
+++ app/src/main/java/wardiman/com/yumna/Quiz/SkorSyhActivity.java
@@ -0,0 +1,15 @@
+package wardiman.com.yumna.Quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wardiman.com.yumna.R;
+
+public class SkorSyhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skor_syh);
+    }
+}
